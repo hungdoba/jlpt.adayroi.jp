0023045ec8981281f637ab899a871464fe0433ae
hungdoba <hungdoba.hdb@gmail.com>
2025-03-10 20:23:39 +0900

    add explanation component and integrate hint functionality in question component

---
+++ src/components/explanation.tsx
@@ -0,0 +1,34 @@
+import { Button } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { CircleHelp } from 'lucide-react';
+
+export default function Explaination() {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <CircleHelp size={16} strokeWidth={0.5} />
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Edit profile</DialogTitle>
+          <DialogDescription>
+            Make changes to your profile here. Click save when you're done.
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter>
+          <Button type="submit">Save changes</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
--- src/components/question.tsx
+++ src/components/question.tsx
@@ -6,6 +6,8 @@ import { RadioGroup, RadioGroupItem } from './ui/radio-group';
 import { Label } from './ui/label';
 import HintIcon from './ui/hint-icon';
 import { useState } from 'react';
+import { CircleHelp } from 'lucide-react';
+import Explanation from './explanation';
 
 type Props = {
   mondaiId: number;
@@ -15,58 +17,64 @@ type Props = {
 export default function Question({ mondaiId, question }: Props) {
   const [showHint, setShowHint] = useState(false);
   return (
-    <div>
-      <div className="mt-4 mb-2 flex items-center justify-between">
-        <h3>
-          {`${question.question_id.replace('question-', '')}. ${
-            question.question_text
-          }`}
-        </h3>
-        <div className="flex">
-          <BookmarkIcon />
-          <HintIcon onClick={(status) => setShowHint(status)} />
+    <>
+      <div>
+        <div className="mt-4 mb-2 flex items-center justify-between">
+          <h3>
+            {`${question.question_id.replace('question-', '')}. ${
+              question.question_text
+            }`}
+          </h3>
+          <div className="flex">
+            <BookmarkIcon />
+            <HintIcon onClick={(status) => setShowHint(status)} />
+          </div>
         </div>
-      </div>
-      <div className="mx-8 flex justify-between">
-        <RadioGroup
-          defaultValue="option-one"
-          className="flex flex-row flex-wrap justify-between w-full"
-        >
-          {question.choices.map((choice, choiceIndex) => (
-            <div
-              key={choiceIndex}
-              className={cn(
-                'flex items-center mb-2',
-                mondaiId == 1 || mondaiId == 2 || mondaiId == 3 || mondaiId == 5
-                  ? 'w-full md:w-1/2 lg:w-1/2'
-                  : mondaiId == 4 ||
-                    mondaiId == 10 ||
-                    mondaiId == 11 ||
-                    mondaiId == 12 ||
-                    mondaiId == 13
-                  ? 'w-full'
-                  : 'w-full md:w-1/2'
-              )}
-            >
-              <RadioGroupItem
-                value={`option-${choiceIndex}`}
-                id={`option-${choiceIndex}-${mondaiId}`}
-              />
-              <Label
-                htmlFor={`option-${choiceIndex}-${mondaiId}`}
+        <div className="mx-8 flex justify-between">
+          <RadioGroup
+            defaultValue="option-one"
+            className="flex flex-row flex-wrap justify-between w-full"
+          >
+            {question.choices.map((choice, choiceIndex) => (
+              <div
+                key={choiceIndex}
                 className={cn(
-                  'ml-2',
-                  showHint && question.answer == choiceIndex
-                    ? 'text-green-500'
-                    : ''
+                  'flex items-center mb-2',
+                  mondaiId == 1 ||
+                    mondaiId == 2 ||
+                    mondaiId == 3 ||
+                    mondaiId == 5
+                    ? 'w-full md:w-1/2 lg:w-1/2'
+                    : mondaiId == 4 ||
+                      mondaiId == 10 ||
+                      mondaiId == 11 ||
+                      mondaiId == 12 ||
+                      mondaiId == 13
+                    ? 'w-full'
+                    : 'w-full md:w-1/2'
                 )}
               >
-                {`${choiceIndex + 1}. ${choice}`}
-              </Label>
-            </div>
-          ))}
-        </RadioGroup>
+                <RadioGroupItem
+                  value={`option-${choiceIndex}`}
+                  id={`option-${choiceIndex}-${mondaiId}`}
+                />
+                <Label
+                  htmlFor={`option-${choiceIndex}-${mondaiId}`}
+                  className={cn(
+                    'ml-2',
+                    showHint && question.answer == choiceIndex
+                      ? 'text-green-500'
+                      : ''
+                  )}
+                >
+                  {`${choiceIndex + 1}. ${choice}`}
+                </Label>
+                {showHint && question.answer == choiceIndex && <Explanation />}
+              </div>
+            ))}
+          </RadioGroup>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
